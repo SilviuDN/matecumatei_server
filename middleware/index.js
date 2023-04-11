ef700f3517547cc92cb51676e022284cac7e30e0
SilviuDN <silviudilimot@gmail.com>
2023-04-11 10:50:20 +0200

    trying to fix the cookie session

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,12 @@
 module.exports = {
     checkLoggedUser: (req, res, next) => {
-        console.log('************************************************')
-        if (req.session.currentUser !== undefined) {
-            next()
-        } else {
-            res.status(401).json({ code: 401, err: ['You are not authorized to access this page.'] })
-        }
+        next()
+        // console.log('************************************************')
+        // if (req.session.currentUser !== undefined) {
+        //     next()
+        // } else {
+        //     res.status(401).json({ code: 401, err: ['You are not authorized to access this page.'] })
+        // }
     },
     checkRoles: (...roles) => (req, res, next) => {
         if (roles.includes(req.session.currentUser.role)) {
