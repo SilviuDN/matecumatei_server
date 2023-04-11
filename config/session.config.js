5bb52d1a083bf5df6bb5a51f5b180def5f985160
SilviuDN <silviudilimot@gmail.com>
2023-04-11 15:13:54 +0200

    trying to fix the cookie session

--- config/session.config.js
+++ config/session.config.js
@@ -3,7 +3,7 @@ const MongoStore = require('connect-mongo');
 
 module.exports = app => {
     const MONGO_URI =  process.env.DB_REMOTE || process.env.MONGODB_URI || "mongodb://localhost/server";
-    app.set('trust proxy', 1) // trust first proxy
+    // app.set('trust proxy', 1) // trust first proxy for production, when secure: true 
     app.use(
         session({
             secret: process.env.SESS_SECRET,
@@ -12,8 +12,8 @@ module.exports = app => {
             // name: 'MateCuMateiCookie', // This needs to be unique per-host.
             cookie: {
                 // sameSite: 'none',
-                sameSite: false,
-                secure: true, //Some web browsers require that the Secure attribute be set to true when the SameSite attribute has been set to 'none'
+                // sameSite: false,
+                // secure: true, //Some web browsers require that the Secure attribute be set to true when the SameSite attribute has been set to 'none'
                 httpOnly: false,
                 // httpOnly: true,
                 maxAge: 6000000,
