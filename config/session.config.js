e0c919ed2ebac987aafd2c2ee4f44e0f301e1feb
SilviuDN <silviudilimot@gmail.com>
2023-04-13 13:02:27 +0200

    trying different session configurations

--- config/session.config.js
+++ config/session.config.js
@@ -7,7 +7,7 @@ module.exports = app => {
     app.use(
         session({
             secret: process.env.SESS_SECRET,
-            resave: true,
+            resave: false,
             saveUninitialized: false,
             // name: 'MateCuMateiCookie', // This needs to be unique per-host.
             cookie: {
